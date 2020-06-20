aa46cf94eeeb97928721f71b7a4f5821c5928025
decay <dmitrysk@ro.ru>
2020-06-20 13:20:25 +0300

    Add artifacts cleanup and compose remaking

--- autogen/generator.py
+++ autogen/generator.py
@@ -11,8 +11,10 @@ import networkx
 import matplotlib.pyplot as plt
 import argparse
 
+from subprocess import check_call
+
 logger = logging.getLogger('generator')
-logger.addHandler(logging.StreamHandler())
+#logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
 logging.debug('Parse argumetns')
@@ -20,11 +22,15 @@ argparser = argparse.ArgumentParser(description='Generate docker-compose file an
 argparser.add_argument('--recreate',
     dest='recreate',
     action='store_true',
-    help='Recreate configs from scratch, if not set - just visualize current')
+    help='Recreate configs from scratch, clean all')
 argparser.add_argument('--no-recreate',
     dest='recreate',
     action='store_false',
-    help='Recreate configs from scratch, if not set - just visualize current')
+    help='Save configs and just visualize current')
+argparser.add_argument('--clean',
+    dest='clean',
+    action='store_true',
+    help='Clean nodes artifacts')
 args = argparser.parse_args()
 
 swd = os.path.dirname(os.path.abspath(__file__))
@@ -34,6 +40,7 @@ node_config_file = os.path.join(swd, "node.conf.j2")
 dc_template_file = os.path.join(swd, "dc.yml.j2")
 node_temp_config_dest = os.path.join(swd, "node-configs")
 dc_outfile = os.path.join(swd, "docker-compose.yml")
+node_shared_artifacts_dir = os.path.normpath(os.path.join(swd, '..', 'artifacts'))
 
 logger.info("Read tool config")
 cfg = yaml.load(open(config_file), Loader=yaml.Loader)
@@ -43,13 +50,18 @@ if args.recreate:
     if os.path.exists(node_temp_config_dest):
         shutil.rmtree(node_temp_config_dest)
     os.makedirs(node_temp_config_dest)
+if args.recreate or args.clean:
+    logger.info("Clean artifacts dir")
+    if os.path.exists(node_shared_artifacts_dir):
+        check_call(['sudo', 'rm', '-rf', node_shared_artifacts_dir])
+    os.makedirs(node_shared_artifacts_dir)
 
-logger.info("Read templates")
+
+logger.debug("Load templates")
 with open(node_config_file, 'r') as f:
     node_j2_cfg_template = jinja2.Template(f.read())
 with open(dc_template_file, 'r') as f:
     dc_j2_template = jinja2.Template(f.read())
-
 
 networks = [f"network{nw_idx}" for nw_idx in range(cfg['networks count'])]
 gateways_count = [0] * cfg['networks count']
@@ -62,7 +74,7 @@ if args.recreate:
         networks.append(nw_name)
         logger.info(f"{nw_idx} | Make pure nodes")
         for n_idx in range(cfg['network peers']):
-            n_name = f"nw{nw_idx}-node{n_idx}"
+            n_name = f"nw{nw_idx}-n{n_idx}"
             nodes.append({
                 'name': n_name,
                 'networks': [nw_name],
@@ -77,7 +89,7 @@ if args.recreate:
             other_nw_idxs.remove(nw_idx)
             for gw_idx in range(random.randint(1, cfg['max gateways'])):
                 gateways_count[nw_idx] += 1
-                gw_name = f"node{len(nodes)}"
+                gw_name = f"gw{len(nodes)}"
                 gw_networks = [nw_name]
                 gw_conn_cnt = random.randint(1, cfg['max gateway connectivity'])
                 for _ in range(gw_conn_cnt):
@@ -92,25 +104,29 @@ if args.recreate:
                     'cfgfile': os.path.join(node_temp_config_dest, gw_name)})
     logger.info(f"Made {len(nodes)} nodes to fulfill config")
 
-    logger.debug("Write nodes config")
-
+    logger.debug("Write node configs")
     for node in nodes:
         output_text = node_j2_cfg_template.render(node)
         with open(node['cfgfile'], 'w') as f:
             f.write(output_text)
 
-    logger.debug(f"Make docker-compose file '{dc_outfile}'")
-
-    with open(dc_outfile, 'w') as dc_file:
-        dc_file.write(dc_j2_template.render({
-            'networks': networks,
-            'nodes': nodes
-        }))
 else:
+
     for n_cfg_fn in os.listdir(node_temp_config_dest):
         with open(os.path.join(node_temp_config_dest, n_cfg_fn)) as n_cfg_f:
-            nodes.append(yaml.load(n_cfg_f, Loader=yaml.Loader))
-    logger.info(f"Read {len(nodes)} nodes to fulfill config")
+            n_cfg = yaml.load(n_cfg_f, Loader=yaml.Loader)
+            n_cfg['cfgfile'] = os.path.join(node_temp_config_dest, n_cfg_fn)
+            nodes.append(n_cfg)
+    logger.info(f"Read {len(nodes)} node configs")
+
+logger.debug(f"(re)Dump docker-compose file '{dc_outfile}'")
+
+with open(dc_outfile, 'w') as dc_file:
+    dc_file.write(dc_j2_template.render({
+        'networks': networks,
+        'nodes': nodes,
+        'node_shared_artifacts_dir': node_shared_artifacts_dir
+    }))
 
 logger.info("Visualize network just made")
 
@@ -127,7 +143,7 @@ plt.axis('off')
 networkx.draw(
     G,
     pos = networkx.nx_pydot.graphviz_layout(G),
-    node_size=200,
+    node_size=100,
     node_color='lightblue',
     linewidths=1,
     font_size=8,
